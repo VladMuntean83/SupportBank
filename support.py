08ca22951756d6b3cc8ce8cfdd57a9dd5758aad1
Vlad Muntean <Vlad.Muntean@softwire.com>
2026-07-16 18:14:57 +0300

    Modified exceptions and format

--- support.py
+++ support.py
@@ -5,14 +5,16 @@ PATH = 'data/Transactions2014.csv'
 def group_transactions(df):
     filtered_df = df[['To', 'From', 'Amount']]
 
-    to_give = filtered_df.groupby(['From']).sum()
-    to_get = filtered_df.groupby(['To']).sum()
+    to_give = filtered_df[['From', 'Amount']].groupby(['From']).sum()
+    to_get = filtered_df[['To', 'Amount']].groupby(['To']).sum()
 
     all_df = (to_give.join(to_get,how='outer',
                           lsuffix='_to_give', rsuffix='_to_get')
               .filter(like='Amount'))
 
-    return all_df
+    all_df['Balance'] = all_df['Amount_to_get'] - all_df['Amount_to_give']
+
+    return all_df[['Balance']]
 
 def search_account(df, account):
     return df.loc[(df['From'] == account) | (df['To'] == account)]
@@ -27,14 +29,14 @@ if __name__ == '__main__':
     while True:
         try:
             cmd = input()
-            elif cmd == 'List All':
+            if cmd == 'List All':
                 print(group_transactions(df))
             elif cmd.startswith('List '):
                 print(search_account(df, cmd[5:]))
             elif cmd == 'exit':
-                raise Exception('Program stopped by user')
+                print('Program stopped by user.')
+                break
             else:
                 print('Invalid command')
         except Exception as e:
-            print('Stopping program...')
-            break
+            print(f'Exception occurred: {e}')
