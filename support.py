6aaebbcfca60fce858b7cb6d37785b8dc059919b
Vlad Muntean <Vlad.Muntean@softwire.com>
2026-07-16 17:40:16 +0300

    Added logging + file selection

---
+++ support.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import logging
+import re
+
+PATH = 'data/Transactions2013.json'
+
+logging.basicConfig(filename='SupportBank.log', filemode='w', level=logging.DEBUG)
+
+def group_transactions(df):
+    filtered_df = df[['To', 'From', 'Amount']]
+    to_give = filtered_df.groupby(['From']).sum()
+    to_get = filtered_df.groupby(['To']).sum()
+
+    all_df = (to_give.join(to_get,how='outer',
+                          lsuffix='_to_give', rsuffix='_to_get')
+              .filter(like='Amount'))
+
+    return all_df
+
+def search_account(df, account):
+    return df.loc[(df['From'] == account) | (df['To'] == account)]
+
+def validate_df(path):
+    df = pd.read_csv(path)
+
+    if df['Amount'].dtype not in ['int64', 'float64']:
+        print('Amount column is not of numeric type. All such rows will be ignored!')
+        logging.debug('Amount column is not of numeric type')
+
+        df = df[df['Amount'].str.match(r"^-?\d+\.\d+$", na=False)]
+
+        df['Amount'] = df['Amount'].apply(float)
+
+    return df
+
+if __name__ == '__main__':
+    print('Starting up...')
+    logging.debug('Starting up...')
+
+
+    df = pd.DataFrame()
+
+    print('Type a command:')
+    while True:
+        try:
+            cmd = input()
+            if cmd.startswith('Import '):
+                df = validate_df('data/' + cmd[7:])
+            elif cmd == 'List All':
+                print(group_transactions(df) if not df.empty else 'Select a file!')
+            elif cmd.startswith('List '):
+                print(search_account(df, cmd[5:]) if not df.empty else 'Select a file!')
+            elif cmd == 'exit':
+                raise Exception('Program stopped by user')
+            else:
+                print('Invalid command')
+        except Exception as e:
+            print('Stopping program...')
+            logging.exception(f'Exception occurred: {e}')
+            break
